c621a25602ad60f4d46bfc28e7b11599070143ac
Vitaliy Emelianov <Vitaliy030589@gmail.com>
2016-05-26 07:22:08 +0400

    make edits on the draft

--- app/dist/js/script.js
+++ app/dist/js/script.js
@@ -8,4 +8,11 @@ $(document).ready(function(){
 	$(".addReview").click(function(){
 		$("#mailUs").css("display", "block");
 	});	
+
+	$(".questContainer").hover(function(){
+		$(this).children('.questPromo').css("display", "block");
+	}, function(){
+		$(this).children('.questPromo').css("display", "none");
+	});
+
 });
